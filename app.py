b5621425d86a331f15edd81bb33daff25b624901
ouakki ayoub <ok.ayoub1998@gmail.com>
2022-12-10 00:10:25 +0100

    version 2

--- app.py
+++ app.py
@@ -6,11 +6,23 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
-@app.route('/tairrain')
+@app.route('/terrain')
 def ter_page():
     items= [
         {'id':1, 'name':'test', 'barcode':'89977788', 'price':10},
         {'id':1, 'name':'test', 'barcode':'89977788', 'price':12},
         {'id':1, 'name':'test', 'barcode':'89977788', 'price':16}
     ]
-    return render_template('ter.html',items=items)
+    return render_template("ter.html",items=items)
+
+@app.route('/signeUp')
+def signeUp_page():
+    return render_template("signeUp.html")
+
+@app.route('/Login')
+def Login_page():
+    return render_template("Login.html")
+
+@app.route('/addTer')
+def add_Ter():
+    return render_template("addTerrain.html")
---
+++ views.py
@@ -0,0 +1,5 @@
+#from flask import Blueprint, render_template
+#view =Blueprint('views',__name__)
+#@view.route('/terrain')
+#def terrain():
+    #return render_template("ter.html")
