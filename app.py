d7aeaba60a705b69b2426b1c148660110da00b07
ouakki ayoub <ok.ayoub1998@gmail.com>
2022-12-17 00:39:03 +0100

    version2

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_DB'] = "stadiumsclick"
+mysql=MySQL(app)
+
+
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template('home.html')
+    return render_template('home.html',username=session['username'])
 
 @app.route('/terrain')
 def ter_page():
@@ -19,9 +28,34 @@ def ter_page():
 def signeUp_page():
     return render_template("signeUp.html")
 
-@app.route('/Login')
-def Login_page():
-    return render_template("Login.html")
+
+@app.route('/club')
+def club_page():
+    return render_template("club.html")
+
+@app.route('/Login',methods=['GET','POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM login WHERE user_name = %s AND password = %s', (username, password,))
+        users = cursor.fetchone()
+        if users:
+            session['loggedin'] = True
+            session['user_name'] = users['username']
+            session['password'] = users['password']
+            # Redirect to home page
+            return 'Logged in successfully!'
+        else:
+            msg = 'Incorrect username/password!'
+            return render_template('home.html', msg=msg)
+        
+    return render_template('login.html', msg=msg)
+        
+
 
 @app.route('/addTer')
 def add_Ter():
